1741ad1d35b83187cfa35c2e44e53647235bab7a
linyuxuanlin <824676271@qq.com>
2020-03-24 13:54:35 +0800

    content fix

--- Python 学习/list & tuple/list.py
+++ Python 学习/list & tuple/list.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 print('列出 list:')
-abc = ['a','b','c']
+abc = ['a', 'b', 'c']
 print(abc)
 
 
@@ -12,8 +12,8 @@ print(len(abc))
 print('索引访问某个元素(从0编号):')
 print(abc[0])
 print(abc[1])
-print(abc[-1]) #最后一个元素
-print(abc[-2]) #倒数第二个元素
+print(abc[-1])  # 最后一个元素
+print(abc[-2])  # 倒数第二个元素
 
 
 print('追加元素到末尾:')
@@ -22,30 +22,20 @@ print(abc)
 
 
 print('在某位置插入元素:')
-abc.insert(1,'insert-somewhere')
+abc.insert(1, 'insert-somewhere')
 print(abc)
 
 
 print('删除末尾元素:')
-print(abc.pop()) #执行一次，同时显示被删除的元素
+print(abc.pop())  # 执行一次，同时显示被删除的元素
 print(abc)
 
 
 print('删除指定元素:')
-print(abc.pop(1)) #执行一次，同时显示被删除的元素
+print(abc.pop(1))  # 执行一次，同时显示被删除的元素
 print(abc)
 
 
 print('替换某个元素:')
 abc[1] = 'B'
 print(abc)
-
-
-
-
-
-
-
-
-
-
--- Python 学习/list & tuple/tuple.py
+++ Python 学习/list & tuple/tuple.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-#元组 tuple.和 list 非常类似，但 tuple 一旦初始化就不能修改
-abc = ['a','b','c']
+# 元组 tuple.和 list 非常类似，但 tuple 一旦初始化就不能修改
+abc = ['a', 'b', 'c']
 print(abc)
 
-#但如果 tuple 中包含 list,则可变
+# 但如果 tuple 中包含 list,则可变
